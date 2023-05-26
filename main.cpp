46084983b0e648b763ded96146aa653d7f0a4531
Buddhika Chaturanga <bc2789@zebra.com>
2023-05-26 15:46:49 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <openssl/sha.h>
+#include <iostream>
+#include <string>
+
+std::string CalculateSHA256(const std::string& data)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char*>(data.c_str()), data.length(), hash);
+
+    std::string sha256Hash;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
+    {
+        char hex[3];
+        sprintf(hex, "%02x", hash[i]);
+        sha256Hash += hex;
+    }
+
+    return sha256Hash;
+}
+
+int main()
+{
+    std::string data = "Hello, World!";
+    std::string sha256Hash = CalculateSHA256(data);
+
+    std::cout << "SHA-256 Hash: " << sha256Hash << std::endl;
+
+    return 0;
+}
